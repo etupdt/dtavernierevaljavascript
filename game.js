45e7c45c5bb6195f680858c16110281fde54c1a5
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-01-13 12:48:32 +0100

    add hold event in javascript

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 
+let round = 1
 
 $(document).ready(() => {
 
@@ -7,9 +8,78 @@ $(document).ready(() => {
 
         e.preventDefault()
 
+
         const diceValue = Math.floor(Math.random() * 6) + 1
 
+        const current1 = document.getElementById('current1')
+        const current2 = document.getElementById('current2')
+        
+        if (diceValue === 1) {
+
+            if (round === 1) {
+                current1.innerText = '0'
+            } else {
+                current2.innerText = '0'
+            }
+
+            switchPlayer()
+
+        } else {
+    
+            if (round === 1) {
+                current1.innerText = (parseInt(current1.innerText) + diceValue)
+            } else {
+                current2.innerText = (parseInt(current2.innerText) + diceValue)
+            }
+        
+        }
+
         displayDice(diceValue)
+
+
+    })
+
+    $('#hold').click(function(e) {
+
+        e.preventDefault()
+        
+        if (game) {
+
+            const current1 = document.getElementById('current1')
+            const current2 = document.getElementById('current2')
+            
+            const global1 = document.getElementById('global1')
+            const global2 = document.getElementById('global2')
+
+            if (round === 1) {
+
+                global1.innerHTML = (parseInt(global1.innerText) + parseInt(current1.innerText))
+                current1.innerText = '0'
+
+                if (parseInt(global1.innerText) >= 10) {
+                    game = false
+                    document.getElementById('dice').className = 'shadow hidden'
+                    document.getElementById('message').innerText = 'Partie terminée et remportée par player 1'
+                } else {
+                    switchPlayer()
+                }
+
+            } else {
+
+                global2.innerText = (parseInt(global2.innerText) + parseInt(current2.innerText))
+                current2.innerText = '0'
+
+                if (parseInt(global2.innerText) >= 10) {
+                    game = false
+                    document.getElementById('dice').className = 'shadow hidden'
+                    document.getElementById('message').innerText = 'Partie terminée et remportée par player 2'
+                } else {
+                    switchPlayer()
+                }
+
+            }
+
+        }
 
     })
 
